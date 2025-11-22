1f2b3d5a03a3e89e278c9b5f2ca684d38587c8b9
5qol255 <149298356+5qol255@users.noreply.github.com>
2025-11-22 11:36:23 +0800

    Add 微机原理4

---
+++ 实验/计算机组成与结构课程设计(Ⅱ)（又称微机原理）/4.asm
@@ -0,0 +1,156 @@
+COM_ADD EQU 0273H 
+PA_ADD EQU 0270H 
+PB_ADD EQU 0271H 
+PC_ADD EQU 0272H 
+_STACK SEGMENT STACK 
+ DW 100 DUP(?) 
+_STACK ENDS 
+_DATA SEGMENT WORD PUBLIC 'DATA' 
+LED_Data	DB	10111110B	;东西绿灯，南北红灯
+		DB	10111111B	;东西绿灯闪烁，南北红灯
+		DB	10111101B	;东西黄灯亮，南北红灯
+		DB	11101011B	;东西红灯，南北绿灯
+		DB	11111011B	;东西红灯，南北绿灯闪烁
+		DB	11011011B	;东西红灯，南北黄灯亮
+
+_DATA ENDS 
+
+CODE SEGMENT 
+START PROC NEAR
+	ASSUME CS:CODE, DS:_DATA, SS:_STACK 
+MOV AX,_DATA
+MOV DS,AX
+NOP 
+;PA、PB、PC为基本输出模式
+MOV DX,COM_ADD
+MOV AL,82H
+OUT DX,AL
+;熄灯 
+MOV DX,PA_ADD 
+MOV AL,0FFH 
+OUT DX,AL
+ 
+INPUT:
+MOV	DX, PB_ADD
+IN	al, DX
+mov     ah, 0
+; 判断
+CMP	al, 0FFH ;11111111
+jz 	Q1_2	
+CMP	al, 0 ;00000000
+jz	Q1_1		
+cmp	al,0feh;11111110
+jz	Q2
+jmp	Q3
+
+Q3:
+mov	dx,PA_ADD
+out	dx,al
+jmp	INPUT
+
+Q1_1:
+mov	dx,PA_ADD
+mov	al,0feh
+mov	ah,0
+Q1_1_loop:
+call	Delay
+out	dx,al
+rol	al,1
+cmp	al,0feh
+jne	Q1_1_loop
+jmp	INPUT
+
+Q1_2:
+mov	dx,PA_ADD
+mov	al,07fh
+mov	ah,0
+Q1_2_loop:
+call	Delay
+out	dx,al
+ror	al,1
+cmp	al,07fh
+je	INPUT
+jmp	Q1_2_loop
+
+Q2:
+mov	dx,PA_ADD
+		LEA	BX,LED_Data
+START1:		MOV	AL,0
+		XLAT
+		OUT	DX,AL		;东西绿灯，南北红灯
+		CALL	DL5S
+		MOV	CX,6
+START2:		MOV	AL,1
+		XLAT
+		OUT	DX,AL		;东西绿灯闪烁，南北红灯
+		CALL	DL500ms
+		MOV	AL,0
+		XLAT
+		OUT	DX,AL
+		CALL	DL500ms
+		LOOP	START2
+		MOV	AL,2		;东西黄灯亮，南北红灯
+		XLAT
+		OUT	DX,AL
+		CALL	DL3S
+		MOV	AL,3		;东西红灯，南北绿灯
+		XLAT
+		OUT	DX,AL
+		CALL	DL5S
+		MOV	CX,6
+START3:		MOV	AL,4		;东西红灯，南北绿灯闪烁
+		XLAT
+		OUT	DX,AL
+		CALL	DL500ms
+		MOV	AL,3
+		XLAT
+		OUT	DX,AL
+		CALL	DL500ms
+		LOOP	START3
+		MOV	AL,5		;东西红灯，南北黄灯亮
+		XLAT
+		OUT	DX,AL
+		CALL	DL3S
+		JMP	INPUT
+		;JMP	START1
+
+
+DL500ms		PROC	NEAR
+		PUSH	CX
+		MOV	CX,60000
+DL500ms1:	LOOP	DL500ms1
+		POP	CX
+		RET
+DL500ms		ENDP
+
+DL3S		PROC	NEAR
+		PUSH	CX
+		MOV	CX,6
+DL3S1:		CALL	DL500ms
+	 	LOOP	DL3S1
+	 	POP	CX
+	 	RET
+	 	ENDP
+			
+DL5S		PROC	NEAR
+		PUSH	CX
+		MOV	CX,10
+DL5S1:		CALL	DL500ms
+	 	LOOP	DL5S1
+	 	POP	CX
+	 	RET
+		ENDP
+
+
+Delay 	PROC NEAR		
+Delay1:	
+XOR	CX,CX
+LOOP	$
+RET
+Delay	ENDP
+
+
+START ENDP 
+CODE ENDS 
+END START
+
